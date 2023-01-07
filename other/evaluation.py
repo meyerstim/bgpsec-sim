a56ba8d5c64cc3cd2c3f8b32b65e3a64edfa6cf5
meyerstim <tm.meyer@t-online.de>
2023-01-07 15:46:40 +0100

    Eval Bug fix
    Values in wrong order

--- other/evaluation.py
+++ other/evaluation.py
@@ -12,32 +12,33 @@ def evaluate(input: str, output: str, threshold: int) -> None:
     length = (100/round(len(data) ** (1. / 3)-1))
 
     deploymentsTierThree = np.arange(0, 101, length)
-    deploymentsTierTwo = np.arange(0, 101, length)
+    deploymentsTierTwo = reversed(np.arange(0, 101, length))
     deploymentsTierOne = np.arange(0, 101, length)
 
-    for deployment in deploymentsTierThree:
-        for deployment2 in deploymentsTierTwo:
+    for deployment2 in deploymentsTierTwo:
+        for deployment in deploymentsTierThree:
             for deployment3 in deploymentsTierOne:
-                area.append([deployment, deployment2, deployment3])
+                area.append([deployment3, deployment, deployment2])
 
     for elements in area:
         x.append(elements[0])
     for elements in area:
-        y.append(elements[1])
+        y.append(elements[2])
     for elements in area:
-        z.append(elements[2])
+        z.append(elements[1])
 
 
     #Evaluate first bare figure
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111, projection='3d')
     ax.grid()
-    image = ax.scatter(x, y, z, c=data, cmap=plt.cm.rainbow)
+    image = ax.scatter(y, z, x, c=data, cmap=plt.cm.rainbow)
     cbar = fig.colorbar(image, shrink=0.6, pad=0.1)
-    cbar.set_label('\n attacker-success-rate (in %)')
-    ax.set_xlabel('Tier One (Deployment-rate in %)')
-    ax.set_ylabel('Tier Two (Deployment-rate in %)')
-    ax.set_zlabel('Tier Three (Deployment-rate in %)')
+    cbar.set_label('\n attacker-success-rate [%]')
+    ax.set_xlabel('Tier Two deployment-rate [%]')
+    ax.set_ylabel('Tier Three deployment-rate [%]')
+    ax.set_zlabel('Tier One deployment-rate [%]')
+    # ax.invert_xaxis()
     plt.savefig(output)
 
     #Cleanup data for second figure
@@ -63,13 +64,13 @@ def evaluate(input: str, output: str, threshold: int) -> None:
     fig2 = plt.figure(figsize=(11, 11))
     aNeu = fig2.add_subplot(111, projection='3d')
     aNeu.grid()
-    imageNeu = aNeu.scatter(xClean, yClean, zClean, c=data_clean, cmap=plt.cm.rainbow)
+    imageNeu = aNeu.scatter(yClean, zClean, xClean, c=data_clean, cmap=plt.cm.rainbow)
     cBarNeu = fig2.colorbar(imageNeu, shrink=0.6, pad=0.1)
-    cBarNeu.set_label('\n attacker-success-rate (in %)')
+    cBarNeu.set_label('\n attacker-success-rate [%]')
     aNeu.set_title('Cleanup shows ' + str(threshold) + '% of all values \n with lowest attacker-success-rate')
-    aNeu.set_xlabel('Tier One (Deployment-rate in %)')
-    aNeu.set_ylabel('Tier Two (Deployment-rate in %)')
-    aNeu.set_zlabel('Tier Three (Deployment-rate in %)')
+    aNeu.set_xlabel('Tier Two deployment-rate [%]')
+    aNeu.set_ylabel('Tier Three deployment-rate [%]')
+    aNeu.set_zlabel('Tier One deployment-rate [%]')
 
     plt.savefig(output+'_cleanUp')
 
